e8286bdd96e72accb881a5a583736a28a4cc5f1c
Jim Young <jimy@jimy.org>
2013-01-13 15:54:33 -0500

    tweaking markup syntax

--- app/models/markup.rb
+++ app/models/markup.rb
@@ -1,4 +1,12 @@
 class Markup < Builder::XmlMarkup
   alias_method :-, :text!
-  alias_method :/, :<<
+  alias_method :<, :<<
+
+  def append_with_leading_space(el)
+    self << ' '
+    self << el
+  end
+
+  alias_method :/, :append_with_leading_space
+  
 end
