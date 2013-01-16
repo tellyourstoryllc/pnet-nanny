00704503c277b369029f9d99070f72804770956d
Jim Young <jimy@jimy.org>
2013-01-16 12:04:01 -0500

    slightly more general logging

--- app/models/peanut/error_log.rb
+++ app/models/peanut/error_log.rb
@@ -1,23 +1,42 @@
 module Peanut::ErrorLog
 
-  def self.log_error(description, bucket=nil)
+  def self.log_event(description, bucket=nil, type=nil)
     unless @skip_logging
       bucket ||= self.class.name
-      UseLog.increment(bucket)
-      Rails.logger.error("ERROR: #{bucket}- #{description}")
+      UseLog.increment("#{bucket}-#{type}")
+      type ||= 'unknown'
+      if type == 'error'
+        Rails.logger.error("#{bucket} #{type}: #{description}")
+      else
+        Rails.logger.info("#{bucket} #{type}: #{description}")
+      end
     end
   end
 
-  def log_error(description, bucket=nil)
+  def self.log_error(d,b)
+    self.log_event(d,b, 'error')
+  end
+
+  def log_event(description, bucket=nil, type=nil)
     unless @skip_logging
       if self.respond_to?(:id)
         bucket ||= "#{self.class.name}:#{self.id}"
       else
         bucket ||= self.class.name
       end
-      UseLog.increment(bucket)
-      Rails.logger.error("ERROR: #{bucket}- #{description}")
+      type ||= 'unknown'
+
+      UseLog.increment("#{bucket}-#{type}")
+      if type == 'error'
+        Rails.logger.error("#{bucket} #{type}: #{description}")
+      else
+        Rails.logger.info("#{bucket} #{type}: #{description}")
+      end
     end
+  end
+
+  def log_error(d,b)
+    self.log_event(d,b, 'error')
   end
 
 end
