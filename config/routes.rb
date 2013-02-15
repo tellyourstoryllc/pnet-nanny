dc3b0bfa2ad73188f43eee7cb6232b80e93912eb
Jim Young <jimy@jimy.org>
2013-02-14 22:40:59 -0500

    fill in some missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,16 @@ PNet::Nanny::Application.routes.draw do
   match 'api/photo/submit' => 'photo_api#submit'
 
   match 'review/:action(/:id)' => 'review'
+  match 'review' => 'review#index'
+
   match 'one_shot/:action(/:id)' => 'admin'
+  match 'one_shot' => 'admin#index'
+
   match 'test/:action(/:id)' => 'dummy'
   
   match 'yum' => 'application#check_cookies'
+
+  root :to => redirect('http://perceptualnet.com')
+  # match '/*path' => redirect('http://perceptualnet.com')
 
 end
