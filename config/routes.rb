65d940bb28a967d23994f5d53f40e2e5d478d6c4
Jim Young <jimy@jimy.org>
2013-02-16 13:56:38 -0500

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 PNet::Nanny::Application.routes.draw do
 
-  match 'mturk/review' => 'mturk#review'
+  match 'login' => 'admin#login', :as => :login
+  match 'register' => 'worker#register'
+
+  match 'mturk/review' => 'mturk#review', :as => :turkey
   match 'mturk/vote' => 'mturk#vote', :as => :vote
 
   match 'api/photo/submit' => 'photo_api#submit'
 
   match 'review/:action(/:id)' => 'review'
-  match 'review' => 'review#index'
+  match 'review' => 'review#index', :as => :review
 
-  match 'one_shot/:action(/:id)' => 'admin'
-  match 'one_shot' => 'admin#index'
+  match 'eden/:action(/:id)' => 'admin'
+  match 'eden' => 'admin#index'
 
   match 'test/:action(/:id)' => 'dummy'
+  match 'bk(/:id)' => 'dummy#submit', :as => :bookmarklet
   
   match 'yum' => 'application#check_cookies'
+
 
   root :to => redirect('http://perceptualnet.com')
   # match '/*path' => redirect('http://perceptualnet.com')
