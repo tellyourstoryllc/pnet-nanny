946ce8ba9a1e3c249e83b8305293c2b8c48a037e
Jim Young <jimy@jimy.org>
2013-02-16 14:23:59 -0500

    add auto increment to all tables

--- db/structure.sql
+++ db/structure.sql
@@ -1,23 +1,23 @@
 CREATE TABLE `clients` (
-  `id` int(20) unsigned NOT NULL,
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `signature` char(16) DEFAULT NULL,
   `status` enum('active','disabled','deleted') DEFAULT NULL,
   `level` smallint(5) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `photo_fingerprints` (
-  `id` bigint(10) unsigned NOT NULL,
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `value` bigint(20) unsigned NOT NULL,
   `created_at` datetime NOT NULL,
   `photo_count` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `fingerprint` (`value`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `photos` (
-  `id` bigint(20) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `client_id` smallint(5) unsigned DEFAULT NULL,
   `url` varchar(250) DEFAULT NULL,
   `fingerprint` bigint(20) unsigned DEFAULT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE `photos` (
   KEY `status` (`status`,`created_at`),
   KEY `fingerprint` (`fingerprint`),
   KEY `url` (`url`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
 
 CREATE TABLE `schema_migrations` (
   `version` varchar(255) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE `schema_migrations` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `use_log_caches` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(250) CHARACTER SET latin1 NOT NULL DEFAULT '',
   `date` date NOT NULL DEFAULT '0000-00-00',
   `count` int(10) unsigned NOT NULL DEFAULT '0',
@@ -44,25 +44,25 @@ CREATE TABLE `use_log_caches` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `use_log_categories` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `parent_id` int(10) unsigned NOT NULL DEFAULT '0',
   `name` varchar(50) NOT NULL,
   `description` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `parent_id` (`parent_id`,`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `use_log_data` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_id` int(10) unsigned NOT NULL DEFAULT '0',
   `date` date NOT NULL DEFAULT '0000-00-00',
   `count` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `date` (`date`,`category_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `votes` (
-  `id` bigint(20) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` datetime NOT NULL,
   `worker_id` int(10) unsigned NOT NULL,
   `photo_id` bigint(20) unsigned NOT NULL,
@@ -71,10 +71,10 @@ CREATE TABLE `votes` (
   `weight` tinyint(3) unsigned NOT NULL DEFAULT '1',
   `correct` enum('pending','yes','no','unknown') NOT NULL DEFAULT 'pending',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `workers` (
-  `id` bigint(20) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `turk_id` bigint(20) unsigned DEFAULT NULL,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(32) DEFAULT NULL,
@@ -88,6 +88,6 @@ CREATE TABLE `workers` (
   `rating` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `turk_id` (`turk_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 INSERT INTO schema_migrations (version) VALUES ('20130111210021');
