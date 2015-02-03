9b7414ceea0da126f2f829efabd8afa339e0dcfc
Brian Ray <brian.ray1@gmail.com>
2015-02-03 18:14:05 -0500

    Accept reject_reasons

--- app/controllers/video_api_controller.rb
+++ app/controllers/video_api_controller.rb
@@ -8,6 +8,7 @@ class VideoApiController < ApiController
     params[:video][:callback_url] ||= params[:callback_url] if params[:callback_url].present?
     params[:video][:passthru] ||= params[:passthru] if params[:passthru].present?
     params[:video][:passthrough] ||= params[:passthrough] if params[:passthrough].present?
+    params[:video][:reject_reasons] ||= params[:reject_reasons] if params[:reject_reasons].present?
 
     if params[:video][:url] !~ URI::regexp || params[:video][:callback_url] !~ URI::regexp
       render :json => { :error => "invalid url" }, :status => 400
